5a9cf05469eb00f9887a958244f8de988105dee1
SoftLookingOtter <sarallinden@gmail.com>
2025-08-04 22:00:21 +0200

    Debugging test

---
+++ Debugging/Debugging/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Starting program...");
+
+                GreetUser(); // <-- Sätt breakpoint här
+
+                Console.WriteLine("Back in Main!");
+            }
+
+            static void GreetUser()
+            {
+                Console.WriteLine("Entering GreetUser...");
+                SayHello();
+                SayGoodbye();
+                Console.WriteLine("Leaving GreetUser...");
+            }
+
+            static void SayHello()
+            {
+                Console.WriteLine("Hello there!");
+            }
+
+            static void SayGoodbye()
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
+    }
+}
