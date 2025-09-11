5ff11512418db6bb88b5fb79ee52a510c7fcd912
SoftLookingOtter <sarallinden@gmail.com>
2025-09-11 21:36:21 +0200

    1st edition

---
+++ Fundamentals/Data Structures/Arrays/MonsterArray/MonsterArray/Program.cs
@@ -0,0 +1,20 @@
+﻿const int Levels = 100;
+const int MinMonsters = 1;
+const int MaxMonsters = 50;
+
+int[] monstersPerLevel = new int[Levels];
+
+var rng = Random.Shared;
+
+// Fyll array med nummer mellan 1-50
+for (int i = 0; i < monstersPerLevel.Length; i++)
+{
+    monstersPerLevel[i] = rng.Next(MinMonsters, MaxMonsters + 1);
+}
+
+// Sortera nummer 
+Array.Sort(monstersPerLevel); // Sorterar i stigande ordning
+
+// Skriv ut 
+Console.WriteLine("Number of monsters in levels: " + string.Join(", ", monstersPerLevel));
+
