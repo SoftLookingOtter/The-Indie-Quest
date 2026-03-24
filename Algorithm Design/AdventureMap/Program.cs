dffb06a8e228af5895dda4a9e2d87bf1a06ca6e8
Sara Lindén <213674017+SoftLookingOtter@users.noreply.github.com>
2026-03-24 20:54:54 +0100

    shift maze down and reserve top rows for trees

--- Algorithm Design/AdventureMap/Program.cs
+++ Algorithm Design/AdventureMap/Program.cs
@@ -5,25 +5,45 @@ class Program
     static char[,] map = null!;
     static int playerX;
     static int playerY;
+    static Random random = new Random();
 
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         // 1. Sätt storlek
-        width = 5;
-        height = 4;
+        width = 39;
+        height = 23;
 
         // 2. Skapa map
         map = new char[width, height];
 
         // 3. Fyll map (enkel bana)
         string[] level =
-        {
-            "#####",
-            "#S  #",
-            "#   #",
-            "#####"
+{
+    "#######################################",
+    "#                                     #",
+    "#                                     #",
+    "#                  S                  #",
+    "#                  #                  #",
+    "#   #########      #      #########   #",
+    "#   #       #      #      #       #   #",
+    "#   # ###   #   #######   #   ### #   #",
+    "#   #   #   #             #   #   #   #",
+    "#   ### #   #####     #####   # ###   #",
+    "#       #       #     #       #       #",
+    "#   #####   ### #     # ###   #####   #",
+    "#   #       #   #######   #       #   #",
+    "#   #   ### #      M      # ###   #   #",
+    "#   #   #   ###############   #   #   #",
+    "#   #   #                     #   #   #",
+    "#   #   #####   #######   #####   #   #",
+    "#   #       #   #     #   #       #   #",
+    "#   ####### #   #     #   # #######   #",
+    "#         # #   #######   # #         #",
+    "#   ##### # #             # # #####   #",
+    "#         #                 #         #",
+    "#######################################"
         };
 
         for (int y = 0; y < height; y++)
@@ -47,37 +67,57 @@ class Program
     }
 
     static void DrawMap()
-{
-    for (int y = 0; y < height; y++)
     {
-        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
         {
-            // spelare
-            if (x == playerX && y == playerY)
+            for (int x = 0; x < width; x++)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("☺");
-            }
-            else
-            {
-                char c = map[x, y];
-
-                // vägg
-                if (c == '#')
+                // spelare
+                if (x == playerX && y == playerY)
                 {
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("☺");
                 }
                 else
                 {
-                    Console.ResetColor();
+                    char c = map[x, y];
+
+                    // träd i de tre översta raderna
+                    if (y < 3 && c == ' ')
+                    {
+                        if (random.Next(4) == 0) // ungefär 25% chans
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("♠");
+                        }
+                        else
+                        {
+                            Console.ResetColor();
+                            Console.Write(" ");
+                        }
+                    }
+                    // vägg
+                    else if (c == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(c);
+                    }
+                    // minotaur
+                    else if (c == 'M')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(c);
+                    }
+                    else
+                    {
+                        Console.ResetColor();
+                        Console.Write(c);
+                    }
                 }
-
-                Console.Write(c);
             }
-        }
 
-        Console.ResetColor();
-        Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine();
+        }
     }
-}
 }
